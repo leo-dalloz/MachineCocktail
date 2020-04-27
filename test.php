757802eeaf85619a3066d0c2d3b77e2703dc7167
leo-dalloz <leo.dalloz@etu.univ-amu.fr>
2020-04-27 14:19:19 +0200

    résolution de bug

--- test.php
+++ test.php
@@ -3,20 +3,21 @@ if(isset($_POST['test']))
 {
 	$test = $_POST['test'];
 	$var = file_get_contents("ListeCocktail.txt");
+	$file = fopen("ListeCocktail.txt", "wb");
 
 	if( $test == 1)
 	{
 		$var = explode(" ", $var);
 		echo($var[0]);
 		array_shift($var);
+		fwrite($file, implode(" ", $var));
 	}
 
 	else if( $test == 0 AND isset($_POST['cocktail']))
 	{
 		$var = $var . $_POST['cocktail'];
+		fwrite($var);
 	}
-		$file = fopen("ListeCocktail.txt", "wb");
-		fwrite($file, implode(" ", $var));
 }
 
 

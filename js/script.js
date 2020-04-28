7990232ac04a25f48ebfc0ff96009b8abfc7881c
leo-dalloz <leo.dalloz@etu.univ-amu.fr>
2020-04-28 15:16:44 +0200

    Ajout du modal + animation valide

---
+++ js/script.js
@@ -0,0 +1,22 @@
+(function(){
+	
+	$(document).ready( function(){
+		$(".btn-fabriquer").click(function(){
+			var cocktailName = this.value;
+			$(".btn-fabriquer-ok").click(function(){
+				console.log(cocktailName);
+				var xhr = new XMLHttpRequest();
+				xhr.open('POST', 'http://leodalloz.alwaysdata.net/test.php');
+				xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+				xhr.send('test=0&cocktail=' + cocktailName);
+
+				$("#animation-valide").show();
+				setTimeout(function(){
+					$("#animation-valide").hide("slow");
+				}, 2000);
+			})
+		})
+	});
+
+
+}) ();

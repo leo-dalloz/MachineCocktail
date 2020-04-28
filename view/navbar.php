1a70ffb634fbe4f522652af5a6b979f2c45ff937
leo-dalloz <leo.dalloz@etu.univ-amu.fr>
2020-04-28 17:10:35 +0200

    nouvelle branch develop

---
+++ view/navbar.php
@@ -0,0 +1,17 @@
+	<!-- En-tete -->
+	<nav class="navbar navbar-expand navbar-light bg-light" style="height: 5rem;">
+  <a class="navbar-brand" href="index.html" style="font-size: 3rem;">Home</a>
+  <div class="collapse navbar-collapse" id="navbarColor03">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item">
+        <a class="nav-link" href="profil.html" style="font-size: 2rem;">Profil</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#" style="font-size: 2rem;">Classement</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+
+
+	<!-- Fin en-tete -->

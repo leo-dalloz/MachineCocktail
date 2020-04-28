928a5a19cded247bd01fdf515a20dc1821d80873
leo-dalloz <leo.dalloz@etu.univ-amu.fr>
2020-04-28 21:34:25 +0200

    debug

--- view/navbar.php
+++ view/navbar.php
@@ -1,13 +1,13 @@
 	<!-- En-tete -->
 	<nav class="navbar navbar-expand navbar-light bg-light" style="height: 5rem;">
-  <a class="navbar-brand" href="../controller/indexC.php" style="font-size: 3rem;">Home</a>
+  <a class="navbar-brand" href="../controller/indexC.php" style="font-size: 15px;">Home</a>
   <div class="collapse navbar-collapse" id="navbarColor03">
     <ul class="navbar-nav mr-auto">
       <li class="nav-item">
-        <a class="nav-link" href="../controller/profileC.php" style="font-size: 3rem;">Profil</a>
+        <a class="nav-link" href="../controller/profileC.php" style="font-size: 13px;">Profil</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="#" style="font-size: 2rem;">Classement</a>
+        <a class="nav-link" href="#" style="font-size: 13px;">Classement</a>
       </li>
     </ul>
   </div>

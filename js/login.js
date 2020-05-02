e72609048d279d44d952a0b3a02793d1e5ff67fe
leo-dalloz <leo.dalloz@etu.univ-amu.fr>
2020-05-02 18:51:32 +0200

    debug

--- js/login.js
+++ js/login.js
@@ -1,25 +1,27 @@
 (function () {
-	'use strict';
-	$(() => {
-		$('#form-login').submit(function (){
-			console.log("test");
-			let form = $(this);
-			$('#messages').fadeOut();
-			$.ajax({
-				url: form.attr('action'),
-				method: form.attr('method'),
-				data: form.serialize(),
-				}).done(function (data) {
-					if (data.success === true){
-						window.location.href = 'http://leodalloz.alwaysdata.net/controller/indexC.php';
-					}
-					else {
-						$('#messages').html(data.message).fadeIn();
-					}
-				}).fail(function(){
-					$('body').html("We have a problem Houston&nbsp;!");
-				});
-			return false;
-		});
+	$(document).ready( function(){
+		'use strict';
+		$(() => {
+			$('#form-login').submit(function (){
+				console.log("test");
+				let form = $(this);
+				$('#messages').fadeOut();
+				$.ajax({
+					url: form.attr('action'),
+					method: form.attr('method'),
+					data: form.serialize(),
+					}).done(function (data) {
+						if (data.success === true){
+							window.location.href = 'http://leodalloz.alwaysdata.net/controller/indexC.php';
+						}
+						else {
+							$('#messages').html(data.message).fadeIn();
+						}
+					}).fail(function(){
+						$('body').html("We have a problem Houston&nbsp;!");
+					});
+				return false;
+			});
+		})
 	})
 })();

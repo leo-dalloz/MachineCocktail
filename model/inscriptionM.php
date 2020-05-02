51b6c35ed37026482a7067214482f78b8fad9f94
leo-dalloz <leo.dalloz@etu.univ-amu.fr>
2020-05-02 22:29:49 +0200

    debug

--- model/inscriptionM.php
+++ model/inscriptionM.php
@@ -23,9 +23,10 @@ $obj = new stdClass();
 $obj->message = 'Veuillez remplir tous les champs.';
 $obj->success = false;
 
-if(!empty($_POST)){
-	if(isset($__POST['pseudo']) AND isset($__POST['prenom']) AND isset($__POST['nom']) AND isset($__POST['password']) AND isset($__POST['passwordVerification']) AND
-			 $__POST['pseudo'] != "" AND $__POST['prenom'] != "" AND $__POST['nom'] != ""  AND $__POST['password'] != ""  AND $__POST['passwordVerification'] != "")
+
+if(isset($__POST['pseudo']) AND isset($__POST['prenom']) AND isset($__POST['nom']) AND isset($__POST['password']) AND isset($__POST['passwordVerification']))
+{
+	if($__POST['pseudo'] != "" AND $__POST['prenom'] != "" AND $__POST['nom'] != ""  AND $__POST['password'] != ""  AND $__POST['passwordVerification'] != "")
 	{
 		if(count($__POST['pseudo']) > 30 OR count($__POST['prenom']) > 30 OR count($__POST['nom']) > 30 OR count($__POST['password']) > 30)
 		{
@@ -45,12 +46,12 @@ if(!empty($_POST)){
 				$obj->message = 'Les deux mots de passe ne correspondent pas';
 			}
 		}
-		else
-		{
-			$obj->message = 'Il faut moins de 30 caractères par champs.';
-		}
-	}	
-}
+	}
+	else
+	{
+		$obj->message = 'Il faut moins de 30 caractères par champs.';
+	}
+}	
 header('Cache-Control: no-cache, must-revalidate');
 header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
 header('Content-type: application/json');

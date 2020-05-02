690ea153f25281bdcf552e9805efd0b5acb6b017
leo-dalloz <leo.dalloz@etu.univ-amu.fr>
2020-05-02 18:09:13 +0200

    test new front login

--- view/loginV.php
+++ view/loginV.php
@@ -5,9 +5,35 @@
     <title>Login</title>
     <script src="../js/jquery-3.4.1.min.js"></script>
     <script src="../js/login.js"></script>
+	<link rel="stylesheet"  href="../css/bootstrap.min.css">
+	<link rel="stylesheet"  href="../css/loinStyle.css">
 </head>
 
 <body>
+
+<div class="wrapper fadeInDown">
+  <div id="formContent">
+    <!-- Tabs Titles -->
+
+    <!-- Icon -->
+    <div class="fadeIn first">
+      <p>Login</p>
+    </div>
+
+    <!-- Login Form -->
+    <form>
+      <input type="text" id="login" class="fadeIn second" name="login" placeholder="login">
+      <input type="text" id="password" class="fadeIn third" name="login" placeholder="password">
+      <input type="submit" class="fadeIn fourth" value="Log In">
+    </form>
+
+    <!-- Remind Passowrd -->
+    <div id="formFooter">
+      <a class="underlineHover" href="#">Forgot Password?</a>
+    </div>
+
+  </div>
+</div>
 <h1 id="title">Connexion</h1>
 <form id="form-login" method="post" action="../model/login.php">
     <input type="text" name="username" placeholder="username"/><br>
